632c844cc356e392a590ec336a35b8998341bb0c
seahoers <f.hanssonhuber@gmail.com>
2026-02-03 13:20:13 +0100

    Rename the test file

--- src/test/java/ContinuousIntegrationServerTest.java
+++ src/test/java/ContinuousIntegrationServerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class ContinuousIntegrationServerTest {
 
     /**
      * Rigorous Test :-)
