5c9e8170785fd491802e58f0cad34ab335a3fba6
Felix Hansson Huber <43926314+seahoers@users.noreply.github.com>
2026-02-05 11:00:54 +0100

    feat #18: Detach ContinuousIntegrationServer from main  (#20)
    - Have ContinuousIntegrationServer as its own class (not main class)
    - Add Main class that uses ContinuousIntegrationServer
    - Adapt ContinuousIntegrationServerTest to automatically start and stop
    server for
    
    fixes #18

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,19 +1,30 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;;
 
-public class ContinuousIntegrationServer {
-    private static final int PORT = 8080 + 18;
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(PORT);
+public class ContinuousIntegrationServer { 
+    private static Server server;
 
+    public ContinuousIntegrationServer(int port) {
+        server = new Server(port);
+    
         ServletContextHandler context = new ServletContextHandler("/");
-
+    
         // Register servlets
         context.addServlet(RootServlet.class, "/");
-
+    
         server.setHandler(context);
+    }
+ 
+    public void start() throws Exception {
 
         server.start();
+    }
+
+    public void join() throws InterruptedException {
         server.join();
+    }
+
+    public void stop() throws Exception {
+        server.stop();
     }
 }
