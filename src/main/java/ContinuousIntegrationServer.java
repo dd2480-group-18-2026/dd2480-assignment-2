7f582f13e274b135cff3daf38fece5ccd17f6ae4
Eliott Tadros <53917558+Telmo26@users.noreply.github.com>
2026-02-05 11:11:53 +0100

    feat #15: Implemented SQL storage for build results (#19)
    Closes #15: This pull requests adds build info storage in an SQLite
    database. It also involved adding a data class for storing build
    information.
    
    The database is automatically created and the table is created if it
    doesn't already exist, otherwise nothing happens. There are also tests
    that test for database behavior using temporary databases that are
    deleted after the test has ran.

--- src/main/java/ContinuousIntegrationServer.java
+++ src/main/java/ContinuousIntegrationServer.java
@@ -1,4 +1,7 @@
 import org.eclipse.jetty.server.Server;
+
+import java.io.IOException;
+
 import org.eclipse.jetty.ee10.servlet.ServletContextHandler;;
 
 public class ContinuousIntegrationServer { 
@@ -8,7 +11,13 @@ public class ContinuousIntegrationServer {
         server = new Server(port);
     
         ServletContextHandler context = new ServletContextHandler("/");
-    
+
+        try {
+            var storage = new Storage("build_history.sqlite");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
         // Register servlets
         context.addServlet(RootServlet.class, "/");
     
---
+++ src/main/java/Storage.java
@@ -0,0 +1,104 @@
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Storage {
+    private Connection databaseConnection;
+    
+    Storage(String databasePath) throws IOException {
+        var sqlite_file = new File(databasePath);
+
+        if (!sqlite_file.exists()) {
+            sqlite_file.createNewFile();
+        }
+
+        var url = "jdbc:sqlite:" + databasePath;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            System.out.println("Connection to SQLite has been established.");
+            this.databaseConnection = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        if (this.databaseConnection != null) {
+            this.initializeDatabase();
+            System.out.println("Database migrations complete.");
+        }
+    }
+
+    public void storeBuildResult(BuildResult buildResults) {
+        String sql = """
+            INSERT INTO history(build_nb, commit_sha, build_output)
+            VALUES(NULL, ?, ?)
+            """;
+
+        try(var stmt = this.databaseConnection.prepareStatement(sql)) {
+            stmt.setString(1, buildResults.commitSHA);
+            stmt.setString(2, buildResults.buildOutput);
+            stmt.execute();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public ArrayList<Integer> getBuildIndexes() {
+        String sql = """
+                SELECT build_nb
+                FROM history
+                """;
+
+        try (var stmt = this.databaseConnection.createStatement()) {
+            ResultSet resultSet = stmt.executeQuery(sql);
+            ArrayList<Integer> buildIndexes = new ArrayList<Integer>();
+
+            while (resultSet.next()) {
+                buildIndexes.add(resultSet.getInt(1)); // We get the index for each row
+            }
+
+            return buildIndexes;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<Integer>();
+        }
+    }
+
+    public BuildResult getBuildResult(Integer buildIndex) throws SQLException {
+        var sql = """
+                SELECT * FROM history
+                WHERE build_nb = (?);
+                """;
+        
+        var stmt = this.databaseConnection.prepareStatement(sql);
+        stmt.setInt(1, buildIndex);
+        var resultSet = stmt.executeQuery();
+
+        if (resultSet.next()) {
+            return new BuildResult(resultSet.getString("commit_sha"), resultSet.getString("build_output"));
+        } else {
+            throw new SQLException(String.format("Invalid build index: %d", buildIndex));
+        }
+    }
+
+    private void initializeDatabase() {
+        var sqlStatement = """
+            CREATE TABLE IF NOT EXISTS history (
+            build_nb INTEGER PRIMARY KEY,
+            commit_sha TEXT NOT NULL,
+            build_output TEXT NOT NULL
+            );
+            """;
+
+        try(var stmt = this.databaseConnection.createStatement()) {
+            stmt.execute(sqlStatement);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
