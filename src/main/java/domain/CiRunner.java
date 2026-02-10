b77ee053612c53c17d037361da2ab95155654c0c
Tim Bergbom <tbergbom@kth.se>
2026-02-10 12:37:22 +0100

    mvn compile done

--- src/main/java/domain/CiRunner.java
+++ src/main/java/domain/CiRunner.java
@@ -10,35 +10,38 @@ import java.nio.file.Paths;
 
 
 public class CiRunner {
-	public CiRunner(String repoURL) throws IOException, InterruptedException {
-		ProcessBuilder cloneBuilder = new ProcessBuilder("git", "clone", repoURL, "../../../tempRepo");
-		cloneBuilder.redirectErrorStream(true);
-		Process cloneProcess = cloneBuilder.start();
-		BufferedReader cloneReader = new BufferedReader(new InputStreamReader(cloneProcess.getInputStream()));
-		String line;
-		cloneProcess.waitFor();
-
-		while ((line = cloneReader.readLine()) != null) {
-			System.out.println(line);
-		}
-
-		System.out.println("Runs");
-		ProcessBuilder cloneBuilder = new ProcessBuilder("git", "clone", repoURL, "../../../tempRepo");
-		cloneBuilder.redirectErrorStream(true);
-		Process cloneProcess = cloneBuilder.start();
-		BufferedReader cloneReader = new BufferedReader(new InputStreamReader(cloneProcess.getInputStream()));
-		String line;
-		cloneProcess.waitFor();
-
-		while ((line = cloneReader.readLine()) != null) {
-			System.out.println(line);
-		}
-
-
-
+	public CiRunner(String repoURL, String repoLOC) throws IOException, InterruptedException {
+		cloneRepo(repoURL, repoLOC);
+		compileRepo(repoLOC);
 		
-		File file = new File("../../../tempRepo");
+		File file = new File(repoLOC);
 		deleteDir(file);
+	}
+
+	private boolean cloneRepo(String repoURL, String repoLOC) throws IOException, InterruptedException {
+		ProcessBuilder cloneBuilder = new ProcessBuilder("git", "clone", repoURL, repoLOC);
+		cloneBuilder.redirectErrorStream(true);
+		Process cloneProcess = cloneBuilder.start();
+
+		int result = cloneProcess.waitFor();
+		if (result == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	private void compileRepo(String repoLOC) throws IOException, InterruptedException {
+		String mavenLOC = repoLOC + "/pom.xml";
+		ProcessBuilder compileBuilder = new ProcessBuilder("mvn.cmd", "compile", "-f", mavenLOC);
+		compileBuilder.redirectErrorStream(true);
+		Process cloneProcess = compileBuilder.start();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(cloneProcess.getInputStream()));
+		int res = cloneProcess.waitFor();
+		System.out.println(res);
+		String line;
+		while((line = reader.readLine()) != null) {
+			System.out.println(line);
+		}
 	}
 
 	private void deleteDir(File file) {
