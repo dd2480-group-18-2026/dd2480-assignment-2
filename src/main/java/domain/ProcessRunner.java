cd495a8f562198aeaee7e421f3e96c9229330a94
augustyvdal <august.yvdal@gmail.com>
2026-02-12 14:38:26 +0100

    domain test2

--- src/main/java/domain/ProcessRunner.java
+++ src/main/java/domain/ProcessRunner.java
@@ -3,37 +3,33 @@ package domain;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
 
-import java.lang.ProcessBuilder;
+public final class ProcessRunner implements IProcessRunner {
 
-/**
- * Implementation of the IProcessRunner interface. Uses ProcessBuilder internally.
- */
-final public class ProcessRunner implements IProcessRunner {
-	/**
-	 * Runs a given process using ProcessBuilder
-	 * 
-	 * @param command The program and parameters to run
-	 * @return The output from running the process and whether it succeeded
-	 */
-	public ProcessResult runProcess(String[] command) throws IOException, InterruptedException {
-		boolean success = false;
-		StringBuilder outputString = new StringBuilder();
-		ProcessBuilder builder = new ProcessBuilder(command);
-		builder.redirectErrorStream(true);
-		Process process = builder.start();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    @Override
+    public ProcessResult runProcess(String[] command) throws IOException, InterruptedException {
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.redirectErrorStream(true);
 
-		int result = process.waitFor();
-		String line;
+        System.out.println("Running command: " + String.join(" ", command));
 
-		while((line = reader.readLine()) != null) {
-			outputString.append(line + "\n");
-		} 
-		if (result == 0) {
-			success = true;
-		}
+        Process process = builder.start();
 
-		return new ProcessResult(outputString.toString(), success);
-	}
+        StringBuilder output = new StringBuilder();
+
+        try (BufferedReader reader =
+                     new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+        }
+
+
+        int exit = process.waitFor();
+
+        return new ProcessResult(output.toString(), exit == 0);
+    }
 }
